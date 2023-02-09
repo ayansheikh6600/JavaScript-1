ff7cb1cce5e5fb47808f6c1d1193d4aaac190938
ayansheikh6600 <ayansheikh6600@email.com>
2023-02-09 22:38:12 +0500

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+var a =  15;
+
+var value1 = a++ + ++a  -  --a;
+//           15  + 17   -  16  = 16;
+var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+//           16  + 17  -  16 + 16  + 15 - 15 - 15 - 15 = 3;
+
+
+console.log(value1);
+console.log(value2);
